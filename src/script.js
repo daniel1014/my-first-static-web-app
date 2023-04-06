bffbe4907b9ee79b2201bd92b154237e163952e5
daniel1014 <chuenlik@hotmail.com>
2023-04-06 16:56:53 +0100

    v2

--- src/script.js
+++ src/script.js
@@ -1,30 +1,40 @@
+// Import required modules
 const express = require('express');
 const bodyParser = require('body-parser');
+
+// Create an instance of Express app
 const app = express();
 
-// Set up body-parser middleware
+// Set up middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Serve static files from public directory
+// Define authorized username and password
+const authorizedUsername = 'Daniel.Wong3@aecom.com';
+const authorizedPassword = 'aecom';
+
+// Serve static files (HTML, CSS)
 app.use(express.static('public'));
 
-// Handle login form submission
+// Define login route
 app.post('/login', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+  const { username, password } = req.body;
 
-  // Perform login validation, e.g., by checking against a database or other authentication mechanism
-  // For demonstration purposes, we'll use hardcoded authorized username and password
-  if (email === 'Daniel.Wong3@aecom.com' && password === 'aecom') {
-    res.json({ success: true, message: 'Login successful!' });
-  } else {
-    res.status(401).json({ success: false, message: 'Invalid email or password.' });
+  // Perform server-side validation
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Both username and password are required' });
   }
+
+  if (username !== authorizedUsername || password !== authorizedPassword) {
+    return res.status(401).json({ error: 'Invalid username or password' });
+  }
+
+  // Authentication successful
+  res.json({ message: 'Login successful' });
 });
 
 // Start the server
-const port = 3000; // You can change this to any port number you prefer
+const port = 3000; // replace with your desired port number
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
